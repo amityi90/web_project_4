e7667a728b7e8cfb38d64ebfaaf5bfb64ef649bb
Amit Mottes <amityi90@gmail.com>
2021-08-29 19:55:47 +0200

    fix: third update

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let popupForm = document.querySelector(".form");
+let popupForm = document.querySelector(".popup");
 let editProfile = document.querySelector(".profile__edit-button");
-let closeButton = document.querySelector(".form__close-button");
-let saveButton = document.querySelector(".form__save-button");
+let closeButton = document.querySelector(".popup__close-button");
+let formContent = document.querySelector(".popup__form");
 let nameProfile = document.querySelector(".profile__name");
 let proffesionProfile = document.querySelector(".profile__proffesion");
-let nameInput = document.querySelector(".form__text-input_content_name");
-let proffesionInput = document.querySelector(".form__text-input_content_profession");
+let nameInput = document.querySelector(".popup__text-input_content_name");
+let proffesionInput = document.querySelector(".popup__text-input_content_profession");
 
 function editButton() {
-    popupForm.classList.remove("form_disable");
+    popupForm.classList.remove("popup_disable");
     nameInput.value = nameProfile.textContent;
     proffesionInput.value = proffesionProfile.textContent;
 }
 
 function closePopup() {
-    popupForm.classList.add("form_disable");
+    popupForm.classList.add("popup_disable");
 }
 
 function handleFormSubmit(evt) {
@@ -26,4 +26,4 @@ function handleFormSubmit(evt) {
 
 editProfile.addEventListener('click', editButton);
 closeButton.addEventListener('click', closePopup);
-saveButton.addEventListener('click', handleFormSubmit);
+formContent.addEventListener('submit', handleFormSubmit);
