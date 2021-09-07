4acc4f8624a20ee3b584da7a42e71d70e67012b7
Amit Mottes <amityi90@gmail.com>
2021-09-07 16:23:18 +0200

    feat: Validating the Edit Profile Form

--- script.js
+++ script.js
@@ -112,3 +112,57 @@ formContentPlace.addEventListener('submit', evt => handlePlaceAdd(evt));
 closeButtonImage.addEventListener('click', evt => closePopup(popupBigScreenImage));
 
 
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+});
+}
+
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("popup__save-button_inactive");
+  } else {
+    buttonElement.classList.remove("popup__save-button_inactive");
+  }
+}
+
+
+
+function showInputError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add("popup__text-input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("popup__text-input-error_active");
+
+}
+
+function hideInputError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove("popup__text-input_type_error");
+  errorElement.classList.remove("popup__text-input-error_active");
+  errorElement.textContent = "";
+}
+
+function isValid(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+function setEventListeners(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll(".popup__text-input"));
+  const buttonElement = formElement.querySelector(".popup__save-button");
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+}
+
+
+
+setEventListeners(popupFormProfile);
